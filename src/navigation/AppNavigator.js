f91d409b26a4374a6740ed0f3c6c75daeb402d6b
Amanda Mesquita <amandamesquita73638@gmail.com>
2025-06-01 22:07:08 -0300

    Corrige nome de RegisterScreen com letra maiúscula

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/auth/LoginScreen";
-import RegisterScreen from "../screens/auth/registerScreen";
+import RegisterScreen from "../screens/auth/RegisterScreen";
 
 const Stack = createNativeStackNavigator();
 
